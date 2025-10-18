36e6603bb6ab77a4e6a9be87671f5614d9f7084c
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-18 01:13:21 +0100

    Add files via upload

---
+++ services/script/index.js
@@ -0,0 +1,6 @@
+import {s3, R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText} from "../shared/utils/r2-client.js";
+import { log } from "../../utils/logger.js";
+export async function runScript({ sessionId, nowISO }) {
+  log.info({ sessionId, nowISO }, "📝 [Script] start");
+  // TODO: connect to your existing script logic
+}
