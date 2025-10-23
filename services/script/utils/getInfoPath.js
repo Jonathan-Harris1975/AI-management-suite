4f50f27285a81e1c4764898cb221b5e60f144707
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-23 05:29:44 +0100

    Delete services/script/utils/getInfoPath.js

--- services/script/utils/getInfoPath.js
+++
@@ -1,9 +0,0 @@
-import {s3, R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText} from "../../shared/utils/r2-client.js";
-// Placeholder for getInfoPath utility
-
-function getInfoPath() {
-  // TODO: Implement actual logic
-  return "/default/info/path";
-}
-
-export default getInfoPath;
