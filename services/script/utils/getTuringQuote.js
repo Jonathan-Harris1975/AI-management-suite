dfeff2ac9f33f86e19a2fb4c1fe1cea37020f4a7
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-26 07:14:18 +0000

    Update getTuringQuote.js

--- services/script/utils/getTuringQuote.js
+++ services/script/utils/getTuringQuote.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 let quotesCache = null;
 
-export default function getTuringQuote() {
+export default function getTuringQuote {
   if (!quotesCache) {
     const quotesPath = path.resolve('utils', 'quotes.txt');
     const raw = fs.readFileSync(quotesPath, 'utf-8');
