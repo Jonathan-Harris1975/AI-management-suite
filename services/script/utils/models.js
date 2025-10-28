9a140ebecfedbc74688aff7f3386bf5de351cb8c
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-28 07:59:11 +0000

    Update models.js

--- services/script/utils/models.js
+++ services/script/utils/models.js
@@ -3,7 +3,7 @@
 import { resilientRequest } from "../../shared/utils/ai-service.js";
 import { getIntroPrompt, getMainPrompt, getOutroPromptFull } from "./promptTemplates.js";
 import { fetchFeedArticles } from "./fetchFeeds.js";
-import { putText, putJson } from "#shared/utils/r2-client.js";
+import { putText, putJson } from "../../shared/utils/r2-client.js";
 import { cleanText } from "./textCleaner.js";
 import { chunkText } from "./chunker.js";
 import { generateEpisodeMeta } from "./podcastHelpers.js";
