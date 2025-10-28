892bb3e7b20caa97b8f91ac1937e0cabca434acc
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-10-28 13:36:45 +0000

    Update index.js

--- services/rss-feed-creator/index.js
+++ services/rss-feed-creator/index.js
@@ -2,7 +2,7 @@
 import Parser from "rss-parser";
 import { info, error } from "#logger.js";
 import { getObjectAsText, uploadBuffer } from "#shared/utils/r2-client.js";
-import { callLLMChat } from "../../shared/utils/ai-service.js";
+import { { resilientRequest } } from "../../shared/utils/ai-service.js";
 import { RSS_PROMPTS } from "./utils/rss-prompts.js";
 
 const parser = new Parser();
