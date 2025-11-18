7b18d855423616b7dff7fdd6fd66c3a972ae43b8
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-18 03:24:51 +0000

    Update podcastProcessor.js

--- services/tts/utils/podcastProcessor.js
+++ services/tts/utils/podcastProcessor.js
@@ -1,4 +1,4 @@
-import logger from "../../service-logger.js";
+import logger from "../service-logger.js";
 const { info, warn, error, debug } = logger;
 // ============================================================
 // 🎵 Modular Podcast Processor (Optimized for Podcast Output)
