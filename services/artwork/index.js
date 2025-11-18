380e64e32918dd26f68463f48d5c1c4a510ce4c7
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-18 03:14:01 +0000

    Update index.js

--- services/artwork/index.js
+++ services/artwork/index.js
@@ -1,4 +1,4 @@
-import logger from "../service-logger.js";
+import logger from "./service-logger.js";
 const { info, warn, error, debug } = logger;
 // services/artwork/index.js
 import express from "express";
