065839dc15cb7eae1b3937201402bbac965b62ac
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-18 03:22:35 +0000

    Update io.js

--- services/tts/utils/io.js
+++ services/tts/utils/io.js
@@ -1,4 +1,4 @@
-import logger from "../../service-logger.js";
+import logger from "../service-logger.js";
 const { info, warn, error, debug } = logger;
 // services/tts/utils/io.js
 // Unified I/O helpers for TTS audio pipeline – central R2 + logger
