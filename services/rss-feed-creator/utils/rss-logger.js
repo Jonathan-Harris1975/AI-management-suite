9b900b749cd05a0997731c50dace3add90e6ea42
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-18 05:17:30 +0000

    Add files via upload

--- services/rss-feed-creator/utils/rss-logger.js
+++ services/rss-feed-creator/utils/rss-logger.js
@@ -35,11 +35,12 @@ export class RssLogger {
   endRun(extra={}){
     stopKeepAlive(this.keepAliveLabel);
     const out={
-      
-      📝feedsProcessed:this.metrics.feedsProcessed,
-      📝itemsFetched:this.metrics.itemsFetched,
-      📝itemsRewritten:this.metrics.rewrittenItems,
-      📝itemsUploaded:this.metrics.itemsUploaded,
+      runId:this.currentRunId,
+      durationMs:Date.now()-this.metrics.startTime,
+      feedsProcessed:this.metrics.feedsProcessed,
+      itemsFetched:this.metrics.itemsFetched,
+      itemsRewritten:this.metrics.rewrittenItems,
+      itemsUploaded:this.metrics.itemsUploaded,
       errors:this.metrics.errors,
       ...extra
     };
