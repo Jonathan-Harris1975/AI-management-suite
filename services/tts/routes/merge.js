faa8dbdedcb3b5146acb7f89e83f4d095b81065f
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-18 03:17:28 +0000

    Update merge.js

--- services/tts/routes/merge.js
+++ services/tts/routes/merge.js
@@ -1,4 +1,4 @@
-import logger from "../../service-logger.js";
+import logger from "../service-logger.js";
 const { info, warn, error, debug } = logger;
 // services/merge/routes/merge.js
 // ============================================================
