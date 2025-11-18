77bc82bad4627594a239f8eec971949ffb742d4f
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-18 03:22:09 +0000

    Update editingProcessor.js

--- services/tts/utils/editingProcessor.js
+++ services/tts/utils/editingProcessor.js
@@ -1,4 +1,4 @@
-import logger from "../../service-logger.js";
+import logger from "../service-logger.js";
 const { info, warn, error, debug } = logger;
 // 🎙️ REBUILT — STABLE EDITING PROCESSOR
 // Crash-resistant multi-stage FFmpeg pipeline
