93736e41340c12f09ddf62ac905687e9fdabb339
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-18 01:57:06 +0000

    Add files via upload

--- services/artwork/utils/io.js
+++ services/artwork/utils/io.js
@@ -1,4 +1,4 @@
-import logger from "../../service-logger.js";
+import logger from "../service-logger.js";
 const { info, warn, error, debug } = logger;
 // services/artwork/utils/io.js (patched)
 import { putObject, putJson } from "#shared/r2-client.js";
