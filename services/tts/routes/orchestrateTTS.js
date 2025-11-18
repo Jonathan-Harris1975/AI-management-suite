9bb64c69f47f60042200446dbc0ac190b7ece7f6
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-18 03:20:43 +0000

    Update orchestrateTTS.js

--- services/tts/routes/orchestrateTTS.js
+++ services/tts/routes/orchestrateTTS.js
@@ -1,4 +1,4 @@
-import logger from "../../service-logger.js";
+import logger from "../service-logger.js";
 const { info, warn, error, debug } = logger;
 // Deprecated: prefer routes/tts.js
 import express from "express";
