bba3240f92a0882a5a908fe85fc44b2444f1d187
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-18 03:20:10 +0000

    Update podcast.js

--- services/tts/routes/podcast.js
+++ services/tts/routes/podcast.js
@@ -1,4 +1,4 @@
-import logger from "../../service-logger.js";
+import logger from "../service-logger.js";
 const { info, warn, error, debug } = logger;
 import {s3, R2_R2_BUCKETS, uploadBuffer, listKeys, getObjectAsText} from "../../shared/utils/r2-client.js";
 import express from "express";
