d2b64c2b284bb652274c9c62e88497d954cc3ad0
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-19 15:48:55 +0000

    Update rss-init.js

--- services/rss-feed-creator/startup/rss-init.js
+++ services/rss-feed-creator/startup/rss-init.js
@@ -1,12 +1,12 @@
 // services/rss-feed-creator/startup/rss-init.js
 import { ensureR2Sources } from "../utils/rss-bootstrap.js";
-import { info, error } from "#logger.js";
+import { info, error ,debug} from "#logger.js";
 
 (async () => {
   try {
-    info("🧠 RSS Init — Ensuring feeds and URLs exist in R2...");
+    debug("🧠 RSS Init — Ensuring feeds and URLs exist in R2...");
     await ensureR2Sources();
-    info("✅ RSS Init complete.");
+    info("🟩 RSS Init complete.");
   } catch (err) {
     error("💥 RSS Init failed", { err: err.message });
   }
