b63060c3ca7b3f174c65fe94a3430de3dda79d31
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-11-19 15:32:47 +0000

    Update tempStorage.js

--- scripts/tempStorage.js
+++ scripts/tempStorage.js
@@ -10,7 +10,7 @@ const TEMP_DIR = path.resolve("/app/tmp");
 
 if (!fs.existsSync(TEMP_DIR)) {
   fs.mkdirSync(TEMP_DIR, { recursive: true });
-  log.info("temp.dir.created", { TEMP_DIR });
+  log.debug("temp.dir.created", { TEMP_DIR });
 }
 
 log.info("temp.dir.verified", { TEMP_DIR });
